8b582d593406c9953e644ba94a3c2bbefa2ba867
ximirial <dania005@mail.ru>
2023-09-08 16:37:38 +0300

    quick fix

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from docopt import docopt
-
-usage = '''
+'''
 Usage:
-    main.py [options] [input [output]]
+    main.py [options] [<input> [<output>]]
 
 Options:
     -o <style>, --output-style <style>      Output style.
     -i --input-separator                    Input separator.
 '''
 
+from docopt import docopt
 
 def output_style(string):
     print(string)
 
 if __name__ == "__main__":
-    args = docopt(usage, options_first=True)
+    args = docopt(__doc__, options_first=True)
 
     print(args)
