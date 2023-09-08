241855b33ba366ecbf8c03a8e689f41c3fb0c609
ximirial <dania005@mail.ru>
2023-09-08 18:59:50 +0300

    Added file reading and writing conditions that are working through parameters

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ Options:
 '''
 
 from docopt import docopt
+from os import path
 
+# Dummy function
 def output_style(string):
     print(string)
 
 if __name__ == "__main__":
+    # Getting parameters and options dictionary then print it (for understanding)
     args = docopt(__doc__, options_first=True)
-
     print(args)
+
+    # Working with options and parameters using conditions
+    if args['<input>'] and path.isfile(args['<input>']):
+        with open(args['<input>'], 'r') as f:
+            print(f.read())
+    elif args['<input>']:
+        print(f'File \"{args["<input>"]}\" does not exist.')
+
+    if args['<output>']:
+        with open(args['<output>'], 'w+') as f:
+            f.write('Success') 
